e136fd23defa43d9f7dd5b5595c440da63217553
debodirno <debodirnochandra@gmail.com>
2016-04-05 22:28:16 +0530

    All but highlighting is left

---
+++ app/src/main/java/xenophobe/com/booklibrary/SearchResults.java
@@ -0,0 +1,34 @@
+package xenophobe.com.booklibrary;
+
+/**
+ * Created by Debodirno on 05-Apr-16.
+ */
+
+public class SearchResults {
+    private String book = "";
+    private String author = "";
+    private String publisher = "";
+
+    public void setBook(String book) {
+        this.book = book;
+    }
+
+    public String getBook() {
+        return book;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+}
